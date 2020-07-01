a03675cdec0af820f102ff8b1258abed58ed3314
oguzhan.kaya <oguzhan.kaya@ting.istanbul>
2020-07-01 17:11:06 +0300

    Inıtial Commit of Dependency Project

---
+++ MicroRabbit.Infra.IOC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+using System;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IOC
+{
+  public class DependencyContainer
+  {
+    public static void RegisterServices(IServiceCollection services)
+    {
+      services.AddTransient<IEventBus, RabbitMQBus>();
+    }
+  }
+}
